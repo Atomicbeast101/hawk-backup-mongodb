4a14aa9ff4b9ee8fa28b0a9395e3953650ad10f5
Atomicbeast101 <adam@brodack.net>
2025-05-11 15:47:37 -0500

    Added code

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# hawk-backup-mongodb
+# Hawk Backup - MongoDB
 
-Simple docker image to auto-backup MongoDB database based on environment variables. Provides option to backup to local directory or to SFTP endpoint and basic alerting. 
+Simple docker image to auto-backup MongoDB database based on environment variables. Provides option to backup to local directory or to SFTP endpoint and basic alerting.
+
+## Features
+
+* 100% configured via environment variables
+* Run standalone along with MongoDB docker container, thus eliminating the need for centralized backup solution.
+* Send backups to local directory (via `/backups`) or to an external SFTP solution.
+<!-- * Send basic alerting (Supported options: webhook, notifiers) -->
+
+## Links
+
+* [Setup](SETUP.md)
---
+++ backup.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+TEMP_BACKUP_DIR="/tmp"
+DATE=`date +"%Y-%m-%d_%H-%M-%S"`
+LOCAL_BACKUP_DIR="/backups"
+EXTRA_FLAGS="--ssl-verify-server-cert=off"
+BACKUP_STATUS="true"
+BACKUP_FAIL_REASON=""
+
+echo "Starting backup..."
+
+# Backup databases
+echo " - Creating database dump..."
+mkdir $TEMP_BACKUP_DIR/$DATE
+mongodump --host $DB_SERVER --port $DB_PORT --username $DB_USERNAME --password $DB_PASSWORD --archive > $TEMP_BACKUP_DIR/$DATE/dump.dump
+if [ $? -ne 0 ]; then
+    echo "ERROR: Unable to dump databases from $DB_SERVER:$DB_PORT!"
+    BACKUP_STATUS="false"
+    BACKUP_FAIL_REASON="$Unable to dump databases from $DB_SERVER:$DB_PORT"
+else
+    zip -r $TEMP_BACKUP_DIR/$DATE.zip $TEMP_BACKUP_DIR/$DATE/
+    echo " - File $TEMP_BACKUP_DIR/$DATE.zip database dump created!"
+fi
+rm -rf $TEMP_BACKUP_DIR/$DATE
+
+# Check if database dump exists
+if [ "$BACKUP_STATUS" = "true" ]; then
+    echo " - Validating that $TEMP_BACKUP_DIR/$DATE.zip exists..."
+    if stat $TEMP_BACKUP_DIR/$DATE.zip > /dev/null 2>&1; then
+        echo " - File $TEMP_BACKUP_DIR/$DATE.zip exists!"
+    else
+        echo "ERROR: $TEMP_BACKUP_DIR/$DATE.zip does not exist! Please check the logs for reasons why it didn't exist."
+        BACKUP_STATUS="false"
+        BACKUP_FAIL_REASON="$TEMP_BACKUP_DIR/$DATE.zip does not exist"
+    fi
+fi
+
+# Move dumped file to final destination
+if [ "$BACKUP_STATUS" = "true" ]; then
+    if [ "$BACKUP_TYPE" = "local" ]; then
+        mv $TEMP_BACKUP_DIR/$DATE.zip $LOCAL_BACKUP_DIR/$DATE.zip
+        echo " - File $TEMP_BACKUP_DIR/$DATE.zip has been moved to $LOCAL_BACKUP_DIR/$DATE.zip!"
+    elif [ "$BACKUP_TYPE" = "sftp" ]; then
+        echo "put $TEMP_BACKUP_DIR/$DATE.zip" | sshpass -p $BACKUP_SFTP_PASSWORD sftp -oStrictHostKeyChecking=no -oPort=$BACKUP_SFTP_PORT "$BACKUP_SFTP_USERNAME@$BACKUP_SFTP_HOST:$BACKUP_SFTP_PATH"
+        if [ $? -ne 0 ]; then
+            echo "ERROR: Unable to upload $TEMP_BACKUP_DIR/$DATE.zip to SFTP endpoint ($BACKUP_SFTP_HOST:$BACKUP_SFTP_PATH/$DATE.zip)!"
+            BACKUP_STATUS="false"
+            BACKUP_FAIL_REASON="$TEMP_BACKUP_DIR/$DATE.zip does not exist"
+        else
+            echo " - File $TEMP_BACKUP_DIR/$DATE.zip has been uploaded to SFTP endpoint ($BACKUP_SFTP_HOST:$BACKUP_SFTP_PATH/$DATE.zip)!"
+        fi
+        rm $TEMP_BACKUP_DIR/$DATE.zip
+    else
+        echo "ERROR: $BACKUP_TYPE is not supported! Supported options: local, sftp"
+        BACKUP_STATUS="false"
+        BACKUP_FAIL_REASON="$BACKUP_TYPE is not supported"
+    fi
+fi
+
+# Alert user of backup failure
+if [ "$BACKUP_STATUS" = "false" ]; then
+    echo "TODO: Add alert code here..."
+fi
+
+echo "Backup completed!"
